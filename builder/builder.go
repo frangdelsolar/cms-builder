131b517b4a21a99c02732a5eb949d7fc3eb07300
Francisco Javier González del Solar <frangdelsolar@gmail.com>
2024-08-18 21:24:30 -0300

    database builder

--- builder/builder.go
+++ builder/builder.go
@@ -8,11 +8,13 @@ type Builder struct {
 	logger       *Logger
 	configReader *viper.Viper
 	config       *BuilderConfig
+	db           *Database
 }
 
 type BuilderConfig struct {
 	*LoggerConfig // logger configuration
 	*ConfigFile   // configfile configuration
+	*DBConfig     // database configuration
 }
 
 func NewBuilder(cfg *BuilderConfig) *Builder {
@@ -40,6 +42,26 @@ func NewBuilder(cfg *BuilderConfig) *Builder {
 	output.configReader = configReader
 
 	return output
+}
+
+// ConnectDB connects to the database using the provided configuration.
+//
+// The config parameter is a pointer to a DBConfig struct that contains the database connection settings.
+// No return values.
+func (b *Builder) ConnectDB(config *DBConfig) {
+	// Remember configuration
+	b.config.DBConfig = config
+
+	// Load database
+	db, err := LoadDB(config)
+	if err != nil {
+		log.Error().Err(err).Msg("Error loading database")
+	}
+
+	// Remember connection
+	b.db = db
+
+	log.Info().Interface("DBConfig", config).Msg("Connected to database")
 }
 
 // GetLogger returns the logger instance associated with the Builder.
---
+++ builder/database.go
@@ -0,0 +1,51 @@
+package builder
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Database struct {
+	*gorm.DB
+}
+
+type DBConfig struct {
+	URL  string // Used for postgres
+	Path string // Used for sqlite
+}
+
+func LoadDB(config *DBConfig) (*Database, error) {
+
+	if config.URL == "" && config.Path == "" {
+		return nil, fmt.Errorf("no database configuration provided")
+	}
+
+	var db *Database
+
+	if config.URL != "" {
+		// Connect to PostgreSQL
+		gormDB, err := gorm.Open(postgres.Open(config.URL), &gorm.Config{})
+		if err != nil {
+			return db, err
+		}
+		return &Database{
+			gormDB,
+		}, nil
+	}
+
+	if config.Path != "" {
+		// Connect to SQLite
+		gormDB, err := gorm.Open(sqlite.Open(config.Path), &gorm.Config{})
+		if err != nil {
+			return db, err
+		}
+		return &Database{
+			gormDB,
+		}, nil
+	}
+
+	return db, fmt.Errorf("no database configuration provided")
+}
