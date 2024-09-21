d2fc4f02452cc746033816fc5dd4da4327ab6b7c
Francisco Gonzalez del Solar <frangdelsolar@gmail.com>
2024-09-21 15:39:09 -0300

    log script

--- scripts/find_logs.py
+++ scripts/find_logs.py
@@ -1,6 +1,7 @@
 import os
 import re
 
+
 class Colors:
     GREEN = "\033[0;32m"
     RED = "\033[0;31m"
@@ -10,8 +11,21 @@ class Colors:
     WHITE = "\033[0m"
     BLACK = "\033[0;30m"
 
+log_colors = {
+    "Debug": Colors.GREEN,
+    "Error": Colors.RED,
+    "Trace": Colors.BLUE,
+    "Info": Colors.CYAN,
+    "Warn": Colors.YELLOW
+}
 
-folder_path = "../builder"
+results_count = {
+    "Debug": 0,
+    "Error": 0,
+    "Trace": 0,
+    "Info": 0,
+    "Warn": 0
+}
 
 
 def read_go_files(folder_path: str) -> list[str]:
@@ -25,13 +39,12 @@ def read_go_files(folder_path: str) -> list[str]:
                 files.append(os.path.join(root, filename))
     return files
 
-def read_file_lines(file_path: str) -> None:
+def find_logs(file_path: str) -> None:
     """
     Reads all lines from the given file and prints them to the console.
     
     :param file_path: The path to the file to read from.
     """
-
     print(f"{Colors.BLACK}Checking: {file_path}{Colors.WHITE}")
     
     line_number = 0
@@ -40,35 +53,54 @@ def read_file_lines(file_path: str) -> None:
     with open(file_path, 'r') as file:
         lines = file.readlines()
 
-
     for line in lines:
         line_number += 1
         match = re.search(pattern, line)
         if match:
             log_level = match.group(1)
-
             print_log(file_path, line_number, line, log_level)
+            results_count[log_level] += 1
 
 
 def print_log(file, line_number, line, log_level):
+    """
+    Prints a log message with the given log level to the console.
 
-    color = {
-        "Debug": Colors.GREEN,
-        "Error": Colors.RED,
-        "Trace": Colors.BLUE,
-        "Info": Colors.CYAN,
-        "Warn": Colors.YELLOW
-    }.get(log_level, Colors.WHITE)  # Default
+    :param file: The file where the log message was found.
+    :param line_number: The line number of the log message in the file.
+    :param line: The line containing the log message.
+    :param log_level: The log level of the log message.
+    """
+    color = log_colors.get(log_level, Colors.WHITE)  # Default
 
     print(f"{file.split('/')[-1]}:{line_number} -> {color}{line.strip()}{Colors.WHITE}")
 
 
-def main():
+def present_results():
+    """
+    Prints the results of the log search to the console.
+    """
+    
+    print("\n\nResults:")
+    for level, count in results_count.items():
+        color = log_colors.get(level, Colors.WHITE)  # Default
+        italic = "\033[3m" if count > 0 else ""
+        print(f"{color}{italic}{level}: {count}")
+    
+    # Have a nice banner saying please review the logs and keep them to a minimum
+    print(f"{Colors.BLACK}\n******************")
+    print(f"{Colors.CYAN}Please review the logs and keep them to a minimum.")
+    print(f"{Colors.BLACK}******************")
 
+
+def main():
+    folder_path = "../builder"
     files = read_go_files(folder_path)
 
     for file in files:
-        read_file_lines(file)
+        find_logs(file)
+
+    present_results()
 
 if __name__ == "__main__":
     main()
