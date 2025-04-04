ec045a61723921476bda74888153d1c53bcdf777
Francisco Gonzalez del Solar <frangdelsolar@gmail.com>
2025-04-04 10:38:54 -0300

    bug fix

--- cms-builder-server/pkg/database/models/database-log.go
+++ cms-builder-server/pkg/database/models/database-log.go
@@ -1,15 +1,13 @@
 package models
 
 import (
-	authModels "github.com/frangdelsolar/cms-builder/cms-builder-server/pkg/auth/models"
 	dbTypes "github.com/frangdelsolar/cms-builder/cms-builder-server/pkg/database/types"
 	"gorm.io/gorm"
 )
 
 type DatabaseLog struct {
 	gorm.Model
-	User         *authModels.User   `json:"user"`
-	UserId       string             `gorm:"foreignKey:UserId" json:"userId"`
+	UserId       string             `json:"userId"`
 	Username     string             `json:"username"`
 	Action       dbTypes.CRUDAction `json:"action"`
 	ResourceName string             `json:"resourceName"`
